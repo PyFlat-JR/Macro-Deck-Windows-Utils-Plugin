4dfc535cc954d0075a899f75c3d0068a97fb43c9
Johannes Rößler <johannes.25406@gmail.com>
2025-08-28 23:53:11 +0200

    Hide Cmd window

--- Actions/CommandlineAction.cs
+++ Actions/CommandlineAction.cs
@@ -38,6 +38,7 @@ public class CommandlineAction : PluginAction
                         WorkingDirectory = workingDirectory,
                         Arguments = "/C " + command,
                         RedirectStandardOutput = saveVariable,
+                        CreateNoWindow = true,
                     }
                 };
                 p.Start();
